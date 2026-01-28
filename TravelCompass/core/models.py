3efb4538d035ac8364346afdad9060445deaadff
Madssse <gigq12841@gmail.com>
2026-01-28 18:51:39 +0700

    Creating a Place table

--- TravelCompass/core/models.py
+++ TravelCompass/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=500, blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    dg2is_id = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        help_text="ID объекта из 2ГИС"
+    )
+
+    def __str__(self):
+        return self.name
