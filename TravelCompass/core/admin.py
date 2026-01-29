1a5874875fb48162e3d6e39e4532c1b0adb25f55
Madssse <gigq12841@gmail.com>
2026-01-29 13:09:24 +0700

    The basic template

--- TravelCompass/core/admin.py
+++ TravelCompass/core/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from .models import Place, Route, RoutePoint
+# # Register your models here.
 
-# Register your models here.
+
+# admin.site.register(Place)
+# admin.site.register(Route)
+# admin.site.register(RoutePoint)
