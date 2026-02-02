9e72832f3c7bef8e530a293f98c527166d0fdb18
Madssse <gigq12841@gmail.com>
2026-02-02 18:27:14 +0700

    Добавление url

--- TravelCompass/users/urls.py
+++ TravelCompass/users/urls.py
@@ -3,7 +3,8 @@ from .views import (
     login_view,
     register_view,
     profile_view,
-    logout_view
+    logout_view,
+    confirm_email_view
 )
 
 urlpatterns = [
@@ -11,4 +12,5 @@ urlpatterns = [
     path('register/', register_view, name='register'),
     path('profile/', profile_view, name='profile'),
     path('logout/', logout_view, name='logout'),
+    path('confirm/<str:token>/', confirm_email_view, name='confirm_email'),
 ]
