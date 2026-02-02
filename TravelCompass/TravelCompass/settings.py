21bb1ef6d0c8c545a0446bb6630f418c8e82d4ca
Madssse <gigq12841@gmail.com>
2026-02-02 17:01:28 +0700

    Настройки для отправки потверждения email

--- TravelCompass/TravelCompass/settings.py
+++ TravelCompass/TravelCompass/settings.py
@@ -126,3 +126,16 @@ STATIC_URL = 'static/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Для подтверждентя EMAIL
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'ПОЧТА'
+EMAIL_HOST_PASSWORD = 'ПАРОЛЬ'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
