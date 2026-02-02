cddcdaf5cb2aa13606adace82a1b0975d23ee28b
Madssse <gigq12841@gmail.com>
2026-02-02 17:19:08 +0700

    Создание модели для токена

--- TravelCompass/users/models.py
+++ TravelCompass/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_token = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.user.username
